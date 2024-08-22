99a4a6981d79de09b536c7425d253dfcf6c93def
Sanjay TS <sanjaytsdev@gmail.com>
2024-08-22 20:07:31 +0530

    Spring Main class

---
+++ src/main/java/com/hiber/hiberdemo/HiberdemoApplication.java
@@ -0,0 +1,13 @@
+package com.hiber.hiberdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiberdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiberdemoApplication.class, args);
+	}
+
+}
