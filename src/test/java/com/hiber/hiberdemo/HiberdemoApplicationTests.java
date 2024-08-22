ae82cf78ce10b7df48c29190d1d2657814a637ac
Sanjay TS <sanjaytsdev@gmail.com>
2024-08-22 20:11:37 +0530

    Completed CRUD USING SPRINGBOOT, HIBERNATE AND POSTGRES

---
+++ src/test/java/com/hiber/hiberdemo/HiberdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiber.hiberdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiberdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
